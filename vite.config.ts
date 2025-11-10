db60113944f279e2d59b02fb3509bc3eae4c5589
Bryan <git@singee.me>
2025-11-10 16:47:38 +0800

    do not include devtools in production build

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 import { nitro } from 'nitro/vite';
+import { devtools } from '@tanstack/devtools-vite';
 
 const config = defineConfig({
   plugins: [
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
+    devtools(),
     tanstackStart(),
     nitro({ config: { preset: 'node-server' } }),
     viteReact(),
